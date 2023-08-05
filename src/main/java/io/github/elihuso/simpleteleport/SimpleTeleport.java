636b51ffd9739cf5cee249985c598fa56d3a1edf
LS-KR <Elihuso@outlook.com>
2023-08-05 23:07:02 +0800

    [+] Add RESISTANCE effect for the player who had been accepted the teleport

--- src/main/java/io/github/elihuso/simpleteleport/SimpleTeleport.java
+++ src/main/java/io/github/elihuso/simpleteleport/SimpleTeleport.java
@@ -9,6 +9,8 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -92,6 +94,7 @@ public final class SimpleTeleport extends JavaPlugin {
                     if (!(target == null)) {
                         target.teleport(player.getLocation());
                         target.sendMessage(ChatColor.GREEN + "Teleport to " + ChatColor.WHITE + player.getName());
+                        target.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 900, 4, true, false, true));
                         buffer.add(v);
                     }
                 }
